68243c77cd8b89f4a6f5a77bcdb11080a5d46fe4
ken <kengineeeer14@gmail.com>
2024-04-14 00:07:56 +0900

    Add 25_matrix_multiplication directory.

---
+++ 25_matrix_operation/matrix_multiplication.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+# first way
+def matrix_multiplication_1(A, B):
+    result = np.zeros((A.shape[0], B.shape[1]))
+    for i in range(A.shape[0]):
+        for j in range(B.shape[1]):
+            for k in range(B.shape[0]):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+
+# second way
+def matrix_multiplication_2(A, B):
+    result = np.zeros((A.shape[0], B.shape[1]))
+    for i, B_col in enumerate(B.T):
+        result[:,i] = A @ B_col
+    return result
+
+# third way
+def matrix_multiplication_3(A, B):
+    result = np.zeros((A.shape[0], B.shape[1]))
+    for i, A_row in enumerate(A):
+        result[i,:] = A_row @ B
+    return result
+
+# fourth way
+def matrix_multiplication_4(A, B):
+    result = np.zeros((A.shape[0], B.shape[1]))
+    for i in range(A.shape[1]):
+        result += A[:, i][:, None] * B[i]
+    return result
+    
+
+# Define the matrices
+A = np.array([[1, 2, 3], [3, 2, 1]])
+B = np.array([[4, 2], [1, 0], [2, 3]])
+
+C1 = matrix_multiplication_1(A, B)
+C2 = matrix_multiplication_2(A, B)
+C3 = matrix_multiplication_3(A, B)
+C4 = matrix_multiplication_4(A, B)
+
+print("\nFirst way: \n", C1)
+print("\nSecond way: \n", C2)
+print("\nThird way: \n", C3)
+print("\nFourth way: \n", C4)
